8af472024f8c41ecc90f74fa3564ab206889f5a8
Pooja-Pj205 <poojashri212005@gmail.com>
2025-06-12 07:03:53 +0530

    changes made

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java .util.*;
+import java.io.*;
+public class Main{
+    public static void main(String[]arg){
+        System.out.println("Hello world");
+        String name = "John";
+        System.out.println(name);
+    }
+}
